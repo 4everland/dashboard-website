6ae0ba91320c331aac8462f202acf88e3266838f
saullary <saullarytzc94@gmail.com>
2022-04-07 16:20:55 +0800

    chore: update email tip

--- src/pages/overview.vue
+++ src/pages/overview.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <v-alert text type="info" dismissible v-if="!userInfo.email">
+      <router-link to="/settings?tab=account_binding&type=3">
+        Please subscribe your email address to receive updates and notices for
+        your account.
+      </router-link>
+    </v-alert>
     <e-tabs :list="list" />
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.userInfo;
+    },
+  },
   data() {
     return {
       list: [
