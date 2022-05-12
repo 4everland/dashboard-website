591c13244d91e46e63b9ccc3e9a08b1de0d1eac7
saullary <saullarytzc94@gmail.com>
2022-05-12 17:45:26 +0800

    chore: add e-chart

--- src/components/bucket/components/bucket-statistics.vue
+++ src/components/bucket/components/bucket-statistics.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>statistics</div>
+  <div>
+    <v-alert text type="error" dense>
+      If you refresh or close the page when the folder is being deleted, the
+      displayed number of deleted files may be inaccurate.
+    </v-alert>
+  </div>
 </template>
 
 <script>
