37a0eae23066f6d6b18a4f168e2bc308f939dcd0
indirasieben <indirasiebenenv79@gmail.com>
2022-05-13 11:27:02 +0800

    chore:change login verify

---
+++ src/components/overview/s-newUserTips.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <v-dialog v-model="showDialog" max-width="680" persistent>
+      <div class="pa-10">
+        <div class="mb-6 text-body1">
+          Your wallet address is detected as being associated with the Github
+          account below, do you want to bind this account for data
+          synchronisation?
+          <br />
+          If so, you will need to verify your Github account.
+        </div>
+        <div class="grey lighten-4 text-center mb-6 text-body1 py-4">
+          <v-icon>mdi-github</v-icon>
+          {{ githubName }}
+        </div>
+        <div class="text-center">
+          <v-btn rounded color="#fff" class="mr-8" @click="showDialog = false"
+            >Create new</v-btn
+          >
+          <v-btn rounded color="#34A9FF" class="white--text" @click="bind"
+            >Bind</v-btn
+          >
+        </div>
+      </div>
+      <div></div>
+    </v-dialog>
+    <v-dialog v-model="showSuccess" max-width="680" persistent>
+      <div class="pa-10">
+        <div class="mb-6 text-body1">
+          Successfully bound your account, please login again.
+        </div>
+
+        <div class="text-center">
+          <v-btn rounded color="#34A9FF" class="white--text" @click="logout"
+            >OK</v-btn
+          >
+        </div>
+      </div>
+      <div></div>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showDialog: false,
+      showSuccess: false,
+      githubName: "",
+    };
+  },
+  mounted() {
+    this.getInfo();
+    this.checkBind();
+  },
+  methods: {
+    async getInfo() {
+      const { data } = await this.$http.get("/flc", {
+        params: {
+          _auth: 1,
+        },
+      });
+      console.log(data);
+      if (data.res) {
+        this.githubName = data.github.name;
+        this.showDialog = true;
+      }
+    },
+    async bind() {
+      const { data } = await this.$http.post(
+        "/bind",
+        {
+          type: 1,
+          apply: localStorage.userInfo.username,
+        },
+        {
+          params: {
+            _auth: 1,
+          },
+        }
+      );
+      if (data.applyR) {
+        location.href = data.applyR;
+      }
+    },
+    async checkBind() {
+      const { code } = this.$route.query;
+      if (!code || code == localStorage.last_github_code) return;
+      localStorage.last_github_code = code;
+      try {
+        await this.$http.get(`/auth/vcode/${code}`, {
+          params: {
+            _auth: 1,
+            type: 1,
+          },
+        });
+        localStorage.token = "";
+        this.showSuccess = true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    logout() {
+      location.reload();
+    },
+  },
+};
+</script>
