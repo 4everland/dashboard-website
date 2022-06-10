91325efdf7d8315d0e5598ba57446c178151f5e0
saullary <saullarytzc94@gmail.com>
2022-06-10 10:51:48 +0800

    chore: update build log

--- src/components/hosting/common/h-status.vue
+++ src/components/hosting/common/h-status.vue
@@ -16,7 +16,7 @@ export default {
     },
     staCls() {
       let cls = "gray";
-      if (this.state == "success") cls = "suc-1";
+      if (this.state == "success" || this.state == "syncing") cls = "suc-1";
       else if (this.state == "running") cls = "warn-1";
       else if (/fail|error|cancel|timeout/i.test(this.state)) cls = "fail-1";
       return cls;
