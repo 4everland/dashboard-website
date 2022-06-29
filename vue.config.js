e5faf9454688f03e3fd8e2ddc29f529f0535c4ef
yepCing <yepCing@gmail.com>
2022-06-29 21:25:43 +0800

    chore:update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = {
   transpileDependencies: ["vuetify"],
   outputDir: "dist",
@@ -9,4 +14,58 @@ module.exports = {
     open: true,
   },
   lintOnSave: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        // maxSize: 1024 * 1024,
+        // minChunks: 10,
+        // maxAsyncRequests: 30,
+        // maxInitialRequests: 30,
+        // enforceSizeThreshold: 50000,
+        // cacheGroups: {
+        //   defaultVendors: {
+        //     test: /[\\/]node_modules[\\/]/,
+        //     priority: -10,
+        //     reuseExistingChunk: true,
+        //   },
+        //   default: {
+        //     minChunks: 2,
+        //     priority: -20,
+        //     reuseExistingChunk: true,
+        //   },
+        // },
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial", // only package third parties that are initially dependent
+          },
+          vuetify: {
+            name: "chunk-vuetify", // split vuetify into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?vuetify(.*)/, // in order to adapt to cnpm
+          },
+          minio: {
+            name: "chunk-minio", // split vuetify into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?minio(.*)/, // in order to adapt to cnpm
+          },
+          aws: {
+            name: "chunk-s3", // split vuetify into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?@aws(.*)/, // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+  },
 };
