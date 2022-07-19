d3d8044ec40fb63f9fe55cca7d67298f817ac39b
saullary <saullarytzc94@gmail.com>
2022-07-19 15:15:17 +0800

    chore: update new user tips

--- src/components/overview/new-user-tips.vue
+++ src/components/overview/new-user-tips.vue
@@ -32,6 +32,8 @@
   </div>
 </template>
 <script>
+import Axios from "axios";
+
 export default {
   data() {
     return {
@@ -117,6 +119,59 @@ export default {
     },
     logout() {
       location.reload();
+    },
+
+    async isSolana() {
+      const isSolana = this.userInfo.solana;
+      if (isSolana) {
+        const Array = await this.getSignaturesForAddress();
+        this.checkBlock(Array);
+      }
+    },
+    async getSignaturesForAddress(signature) {
+      // const account = this.userInfo.solana.address;
+      // console.log(account);
+      let Param = {
+        limit: 1000,
+        commitment: "confirmed",
+      };
+      if (signature) {
+        Param.before = signature;
+      }
+      try {
+        const { data } = await Axios.post(process.env.VUE_APP_SOLANA_URL, {
+          jsonrpc: "2.0",
+          id: 1,
+          method: "getSignaturesForAddress",
+          params: ["Vote111111111111111111111111111111111111111", Param],
+        });
+        const array = data.result;
+        return array;
+      } catch (error) {
+        console.log(error);
+        setTimeout(async () => {
+          const signature = this.signature;
+          const Array = await this.getSignaturesForAddress(signature);
+          this.checkBlock(Array);
+        }, 11000);
+      }
+    },
+    async checkBlock(array) {
+      if (!array) {
+        return;
+      }
+      const length = array.length;
+      let flag = array.find((element) => {
+        return element.slot <= 135140758;
+      });
+      if (flag) {
+        console.log(flag);
+      } else {
+        const signature = array[length - 1].signature;
+        this.signature = signature;
+        const Array = await this.getSignaturesForAddress(signature);
+        this.checkBlock(Array);
+      }
     },
   },
 };
