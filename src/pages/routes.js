034f8d64b99f4206dd2c6d6fa347f8653efa61f5
saullary <saullarytzc94@gmail.com>
2022-07-20 15:20:41 +0800

    chore: remove plan page

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,7 +2,6 @@ import Home from "./home";
 import Login from "./login";
 
 import Overview from "./overview";
-import Plan from "./plan";
 
 import HostingRoutes from "./hosting/routes";
 import BucketRoutes from "./bucket/routes";
@@ -36,12 +35,4 @@ export default [
   ...UserRoutes,
   ...MoreRoutes,
   ...UsageRoutes,
-  {
-    path: "/plan",
-    component: Plan,
-    meta: {
-      title: "Plan",
-      wrapCls: "main-wrap pa-0",
-    },
-  },
 ];
