8e5f3a91b04a19cfd1c4afe627ccf073fc3a814a
saullary <saullarytzc94@gmail.com>
2022-08-09 13:17:59 +0800

    chore: update cut string

--- src/plugins/extend.js
+++ src/plugins/extend.js
@@ -101,7 +101,7 @@ String.prototype.capitalize = function () {
 };
 
 String.prototype.cutStr = function (pre = 100, trail = 0) {
-  if (this.length <= pre + trail) return this;
+  if (this.length <= pre + 3 + trail) return this;
   let txt = this.substr(0, pre) + "...";
   if (trail) txt += this.substr(-trail);
   return txt;
