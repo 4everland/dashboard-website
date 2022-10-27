0e466bb59491fae0c9df0a0ff16104e1070e699a
saullary <saullarytzc94@gmail.com>
2022-10-27 16:37:28 +0800

    chore: update overview

--- src/pages/overview.vue
+++ src/pages/overview.vue
@@ -61,3 +61,58 @@
     <new-user-tips />
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+import { newUserDrop } from "@/plugins/airDrop/index.js";
+
+export default {
+  computed: {
+    ...mapState({
+      noticeMsg: (s) => s.noticeMsg,
+    }),
+    uname() {
+      const info = this.$store.state.userInfo;
+      if (info.username) return "Hi " + info.username.cutStr(6, 4);
+      return "Overview";
+    },
+  },
+  watch: {
+    noticeMsg({ name }) {
+      if (name == "close-new-drop") {
+        this.checkReward();
+      }
+    },
+  },
+  async mounted() {
+    try {
+      const isPop = await newUserDrop();
+      if (!isPop) {
+        this.checkReward();
+      }
+    } catch (error) {
+      //
+    }
+  },
+  methods: {
+    async checkReward() {
+      try {
+        const { data } = await this.$http.get("$auth/poster/rewardhub");
+        if (data)
+          this.$confirm(
+            `<p>Dear 4EVERLAND user,</p><div class="lh-2 fz-14 mt-5 mb-5">We are launching Reward Hub to offer you a better experience with products and services. All new users can get free resources (storage, bandwidth, etc.) by completing the following tasks, while users who have already completed the tasks and received the free giveaway resources can simply go to Reward Hub to claim the resources. <p class="mt-2">Having fun while exploring 4EVERLAND! Please feel free to contact us in our communities if you have any questions. </p></div>`,
+            "",
+            {
+              confirmText: "View",
+              hideTitle: true,
+            }
+          ).then(() => {
+            this.$navTo("/reward-hub");
+          });
+      } catch (error) {
+        //
+      }
+    },
+  },
+};
+</script>
