4c023f3800c0485852341334d38f7ba24dfbe22c
indirasieben <indirasiebenenv79@gmail.com>
2023-01-13 16:20:47 +0800

    chore:add disconnect github

--- src/views/settings/st-account.vue
+++ src/views/settings/st-account.vue
@@ -12,6 +12,15 @@
           </div>
           <div class="mt-3 d-flex al-c">
             <v-btn
+              v-if="it.type == 1 && it.account"
+              color="primary"
+              small
+              min-width="75"
+              @click="onDisconnect(it)"
+              >Disconnect</v-btn
+            >
+            <v-btn
+              v-else
               color="primary"
               :disabled="!!it.account"
               small
@@ -316,6 +325,33 @@ export default {
         }
       } catch (error) {
         console.log(error);
+      }
+    },
+    async onUnbind(it) {
+      const type = it.type;
+      const { data } = await this.$http.post(`$auth/unbind`, {
+        type,
+      });
+      console.log(data);
+      this.$toast("Disconnect" + item.title + " successfully");
+    },
+    async onDisconnect(it) {
+      const info = this.userInfo;
+      if (info.wallet) {
+        this.$confirm(
+          "You are about to unbind a GitHub account. After then, the projects in Hosting will no longer be updated and maintained, and you won't be able to access the dashboard via GitHub. Please confirm before proceeding.",
+          "Disconnect Github",
+          {
+            confirmText: "OK",
+          }
+        ).then(() => {
+          this.onUnbind(it);
+        });
+      } else {
+        this.$alert(
+          "Failed to unbind a GitHub account, please try again after binding any wallet address.",
+          "Alert"
+        );
       }
     },
     async exchangeCode(type) {
