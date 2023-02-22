4afa7ce0d3f7a90de87edcf6a4faf9d8f8a8743d
yepCing <yepCing@gmail.com>
2023-02-22 11:39:14 +0800

    chore: update app

--- src/App.vue
+++ src/App.vue
@@ -71,16 +71,15 @@ export default {
   async mounted() {
     const token = localStorage.token;
     if (!token) return;
-    // await this.getNewUser();
-    // if (this.newUserData && this.showGuide) {
-    //   if (this.$route.path != "/overview" && this.$route.path != "/") {
-    //     this.$router.replace("/");
-    //   }
-    //   setTimeout(() => {
-    //     this.$refs.guide.guide();
-    //   }, 2000);
-    // }
-    this.$refs.guide.guide();
+    await this.getNewUser();
+    if (this.newUserData && this.showGuide) {
+      if (this.$route.path != "/overview" && this.$route.path != "/") {
+        this.$router.replace("/");
+      }
+      setTimeout(() => {
+        this.$refs.guide.guide();
+      }, 2000);
+    }
   },
   methods: {
     async getNewUser() {
