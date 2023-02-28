07bf75033b0b7475d7db216f599b93b2e0ccb739
indirasieben <indirasiebenenv79@gmail.com>
2023-02-28 15:59:50 +0800

    text: dashboard text change

--- src/views/account/account-permission.vue
+++ src/views/account/account-permission.vue
@@ -90,7 +90,7 @@ export default {
         {
           title: "Account",
           type: "MEMBER",
-          subs: ["Members", "Permissions Setting"],
+          subs: ["Members", "Configurations"],
         },
       ],
     };
