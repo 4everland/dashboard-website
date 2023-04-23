6228dfabe3ad2942b66c0508a5e99be84d93962d
yepCing <yepCing@gmail.com>
2023-04-23 14:31:08 +0800

    chore: update hosting ui

--- src/views/hosting/new/new-step-0-web3-tpl.vue
+++ src/views/hosting/new/new-step-0-web3-tpl.vue
@@ -134,12 +134,13 @@ export default {
 </script>
 <style lang="scss" scoped>
 .garlxy {
-  z-index: 999;
+  z-index: 99;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.5);
   backdrop-filter: blur(10px);
+  border-radius: 10px;
 }
 </style>
