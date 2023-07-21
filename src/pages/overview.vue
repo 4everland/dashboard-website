3f884ce08def6d934f4d1a548316f7042038d0ed
yepCing <yepCing@gmail.com>
2023-07-21 17:59:30 +0800

    chore: update overview banner

--- src/pages/overview.vue
+++ src/pages/overview.vue
@@ -132,22 +132,6 @@ export default {
       return "Overview";
     },
     banners() {
-      if (!this.onChain) {
-        return [
-          {
-            img: "https://static1.4everland.org/img/banner/2023523-180202.jpg",
-            href: "https://discord.com/channels/852482727164117022/1104253691676065893",
-          },
-          {
-            img: "https://static1.4everland.org/img/banner/2023516-181010.jpg",
-            href: "https://firstsowing.4everland.org",
-          },
-          // {
-          //   img: "https://static1.4everland.org/img/banner/20230420-173710.png",
-          //   to: "/reward-hub",
-          // },
-        ];
-      }
       return [
         {
           img: "https://static1.4everland.org/img/banner/2023523-180202.jpg",
@@ -156,6 +140,10 @@ export default {
         {
           img: "https://static1.4everland.org/img/banner/2023516-181010.jpg",
           href: "https://firstsowing.4everland.org",
+        },
+        {
+          img: "https://static1.4everland.org/img/banner/20230721-174716.png",
+          href: "https://forms.gle/SAzZ2Sw31atnSSsB8",
         },
       ];
     },
