2baeb724b4b7af78259fdb8ce0b0c0f368af86ca
yepCing <yepCing@gmail.com>
2023-08-17 14:33:05 +0800

    chore: update banner

--- src/pages/overview.vue
+++ src/pages/overview.vue
@@ -147,6 +147,10 @@ export default {
     banners() {
       return [
         {
+          img: "https://static1.4everland.org/img/banner/20230815-153912.png",
+          href: "https://unleashar.4everland.org/",
+        },
+        {
           img: "https://static1.4everland.org/img/banner/20230721-174716.png",
           href: "https://forms.gle/SAzZ2Sw31atnSSsB8",
         },
