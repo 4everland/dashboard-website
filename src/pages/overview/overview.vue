c5ee20bbfb2c4bb25e5d0de8b7277f9f53a9ca20
yepCing <yepCing@gmail.com>
2023-12-13 14:25:19 +0800

    chore: update banner

--- src/pages/overview/overview.vue
+++ src/pages/overview/overview.vue
@@ -229,6 +229,10 @@ export default {
     banners() {
       return [
         {
+          img: "https://overview-banner.4everland.store/land.png",
+          href: "https://medium.com/4everland/embracing-efficiency-and-innovation-introducing-land-as-the-unified-payment-unit-on-4everland-3a935f6b0d9e",
+        },
+        {
           // img: "https://4ever-web.4everland.store/img/banner/20230721-174716.png",
           img: "https://overview-banner.4everland.store/feedback.png",
           to: "/bucket/storage/",
