b187ee444c9f24fd5828674079efa1c2cc521e3d
yepCing <yepCing@gmail.com>
2024-01-18 18:11:23 +0800

    chore: update overview

--- src/pages/overview/overview.vue
+++ src/pages/overview/overview.vue
@@ -60,7 +60,7 @@
 
               <div class="al-c mt-6">
                 <div
-                  class="deposite-btn mr-4"
+                  class="deposite-btn"
                   @click="$router.push('/billing/deposit')"
                   v-ripple
                 >
@@ -437,7 +437,7 @@ export default {
       .deposite-btn,
       .conversion-btn {
         border-radius: 4px;
-        padding: 12px 32px;
+        padding: 12px 48px;
         cursor: pointer;
       }
       .deposite-btn {
