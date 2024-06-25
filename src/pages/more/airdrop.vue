1bb5df125cb6038cbc75ce5284f715d41969c825
yepCing <yepCing@gmail.com>
2024-06-25 11:06:55 +0800

    chore: update airdrop

--- src/pages/more/airdrop.vue
+++ src/pages/more/airdrop.vue
@@ -168,7 +168,7 @@
                 </div>
                 <div class="mt-8 al-c space-btw">
                   <div class="reward fz-20 fw-b">
-                    Shared Reward of 5 Million Points
+                    Shared Reward of 500 Thousand Points
                   </div>
                   <v-btn color="#039CFF" @click="handleToTge">
                     <span class="fw-b" style="color: #fff">Let's Go</span>
