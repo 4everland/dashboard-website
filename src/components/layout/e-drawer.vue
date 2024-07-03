3fe2a09f27a6c142af6bcd37d39c00a67af749fd
indirasieben <indirasiebenenv79@gmail.com>
2024-07-03 10:49:01 +0800

    update 4.0

--- src/components/layout/e-drawer.vue
+++ src/components/layout/e-drawer.vue
@@ -1,8 +1,5 @@
 <style lang="scss">
 .e-drawer {
-  // box-shadow: 5px 0px 30px 0px rgba(0, 0, 0, 0.1);
-  z-index: 999;
-
   .v-navigation-drawer__border {
     display: none;
   }
@@ -38,7 +35,7 @@
     }
   }
   .v-list-item--active {
-    color: #775da6;
+    color: var(--v-primary-base);
     background: none;
 
     &.sub::before {
