efa505a3d5f785fb96bf0b686144404128d808d6
yepCing <yepCing@gmail.com>
2024-08-19 20:34:51 +0800

    chore: update task

--- src/pages/booster/components/task-drawer.vue
+++ src/pages/booster/components/task-drawer.vue
@@ -314,13 +314,9 @@ export default {
             this.asMobile ? (location.href = shareUrl) : window.open(shareUrl);
           } else {
             if (data.action.web.message) {
-              if (this.isTgMiniApp) {
-                if (this.asMobile) {
-                  return (location.href = data.action.web.message);
-                } else {
-                  return window.open(data.action.web.message);
-                }
-              }
+              if (this.isTgMiniApp)
+                return this.$tg.openAuto(data.action.web.message);
+
               this.asMobile
                 ? (location.href = data.action.web.message)
                 : window.open(data.action.web.message);
