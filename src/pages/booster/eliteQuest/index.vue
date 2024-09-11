5acd552a06e90373187e90d3292af0a48b94737a
yepCing <yepCing@gmail.com>
2024-09-11 16:30:33 +0800

    chore: update elite quest

--- src/pages/booster/eliteQuest/index.vue
+++ src/pages/booster/eliteQuest/index.vue
@@ -26,7 +26,7 @@
               :class="{ disabled: item.done }"
               @click="handleToLink(item)"
             >
-              {{ item.done ? "End" : item.link ? "Let's Go" : "Claim" }}
+              {{ item.done ? "Ended" : item.link ? "Let's Go" : "Claim" }}
             </div>
           </div>
         </div>
