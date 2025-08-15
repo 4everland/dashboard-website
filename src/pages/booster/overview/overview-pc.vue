ba7743b1e9b612aa73f084edf734e634c8b7538a
haifun <haifuncn@gmail.com>
2025-08-15 11:39:07 +0800

    chore: modify pc img

--- src/pages/booster/overview/overview-pc.vue
+++ src/pages/booster/overview/overview-pc.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    
+    <div class="user-card-airdrop-banner">
+      <img src="/img/airdrop/4ever_banner2.png" width="256" @click="gotoAirdrop">
+    </div>
     <div class="user-card d-none d-md-block">
       <div class="user-card-item fz-12">
         <div
@@ -464,7 +466,7 @@ export default {
 .user-card {
   position: absolute;
   right: 20px;
-  top: 86px;
+  top: 195px;
   padding: 16px;
   width: 264px;
   background: url("/img/booster/user_card_bg.png") no-repeat;
